f40a53f2f12f99a89ab0d611b27af408877585e0
Exentio <sendnudes@exentio.sexy>
2022-11-27 01:05:02 +0100

    Updated to Tachiyomi 0.14.2

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsLibraryScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsLibraryScreen.kt
@@ -205,6 +205,9 @@ class SettingsLibraryScreen : SearchableSettings {
                     title = stringResource(R.string.pref_library_update_interval),
                     entries = mapOf(
                         0 to stringResource(R.string.update_never),
+                        3 to stringResource(R.string.update_3hour),
+                        4 to stringResource(R.string.update_4hour),
+                        6 to stringResource(R.string.update_6hour),
                         12 to stringResource(R.string.update_12hour),
                         24 to stringResource(R.string.update_24hour),
                         48 to stringResource(R.string.update_48hour),
